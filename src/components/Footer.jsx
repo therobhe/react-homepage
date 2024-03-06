4a7d7255240eb0ad991ab4385e24fd2e16df4158
Robert Heinemann <robert.heinemann@s2015.tu-chemnitz.de>
2024-03-06 21:30:16 +0100

    Main & Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export const Footer = () => {
+const year = new Date().getFullYear()
+
     return (
         <footer>
-            Footer
+            <div className="text-center">
+                <small className="copyright">&copy; Robert Heinemann | {year}</small>
+            </div>
         </footer>
     )
 }
