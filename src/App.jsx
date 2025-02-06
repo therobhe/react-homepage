c2ff061ef6d187cde60d0280c6c94055d418ace8
robhe <rheinemann97@gmail.com>
2025-02-06 11:17:30 +0100

    Add some dummy components + style header + add icons + introduce page background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from './components/Header.jsx';
+
 function App() {
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+            <Header/>
+        </>
     )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import logo from '../assets/kevinRushLogo.png';
+import {FaLinkedinIn} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+
+export default function Header() {
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img src={logo} alt="Robert Heinemann Logo"/>
+            </div>
+            <div className="icons m-8 flex items-center justify-center gap-4 text-2xl">
+                <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+                    <FaLinkedinIn />
+                </a>
+                <a href="https://www.github.com" target="_blank" rel="noreferrer">
+                    <FaGithub />
+                </a>
+            </div>
+        </nav>
+    )
+}
