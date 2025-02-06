8c0773f02376f93dcab3033a6ed70e72579afe40
robhe <rheinemann97@gmail.com>
2025-02-06 12:03:30 +0100

    Outsourcing text to own .js

--- src/data/data.js
+++ src/data/data.js
@@ -3,10 +3,6 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-/* AboutMe */
-export const ABOUTME_NAME = `Robert Heinemann`;
-export const ABOUTME_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-
 /* Experiences */
 export const EXPERIENCES = [
   {
@@ -70,10 +66,3 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
-
-/* Footer */
-export const FOOTER = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
