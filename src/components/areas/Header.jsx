9e0ff63c10d88af8c6163b27c986d40da2b64c46
robhe <rheinemann97@gmail.com>
2025-02-06 12:49:10 +0100

    Added individual logo & favicon

--- src/components/areas/Header.jsx
+++ src/components/areas/Header.jsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/kevinRushLogo.png';
+import logo from '../../assets/rh_logo.png';
 import {FaLinkedinIn} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 
@@ -7,7 +7,7 @@ export default function Header() {
         <header>
             <nav className="mb-20 flex items-center justify-between py-6">
                 <div className="flex flex-shrink-0 items-center">
-                    <img src={logo} alt="Robert Heinemann Logo" className="mx-2 w-10"/>
+                    <img src={logo} alt="Robert Heinemann Logo" className="mx-2 w-20"/>
                 </div>
                 <div className="icons m-8 flex items-center justify-center gap-4 text-2xl">
                     <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
