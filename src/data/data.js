93296de0f37dc7813f0e0502a0d8a3ef7f851479
robhe <rheinemann97@gmail.com>
2025-07-31 18:29:19 +0200

    Added musterstadt project and refactoring of images

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,9 @@
-/*import project1 from "../assets/projects/project-1.jpg";*/
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import apocom from "../assets/projects/apocom.jpeg";
-/*import project4 from "../assets/projects/project-4.jpg";*/
+/*import project1 from "../assets/projects/project-1.webp";*/
+/*import project2 from "../assets/projects/project-2.webp";*/
+import project3 from "../assets/projects/project-3.webp";
+import apocom from "../assets/projects/apocom.webp";
+import musterstadt from "../assets/projects/musterstadt.webp";
+/*import project4 from "../assets/projects/project-4.webp";*/
 
 /* Experiences */
 export const EXPERIENCES = [
@@ -26,26 +27,32 @@ export const PROJECTS = [
     id: "apocom",
     image: apocom,
     technologies: ["React", "Bootstrap5", "PHP", "HTML5"],
-    link: "https://apo.com/"
+    links: ["https://apo.com/"]
+  },
+  {
+    id: "musterstadt",
+    image: musterstadt,
+    technologies: ["React", "React-Router", "TypeScript", "TailwindCSS"],
+    links: ["https://musterstadt-community-mockup.netlify.app/", "https://github.com/therobhe/community-webpage"]
   },
   {
     id: "portfolio",
     image: project3,
     technologies: ["React", "TailwindCSS", "FramerMotion"],
-    link: "https://github.com/therobhe/react-homepage"
+    links: ["https://github.com/therobhe/react-homepage/"]
   },
-  {
+/*  {
     id: "task-manager",
     image: project2,
     technologies: ["React", "TailwindCSS"],
-    link: "https://github.com/therobhe/project-managing-tool"
-  }
+    links: ["https://github.com/therobhe/project-managing-tool/"]
+  }*/
   /*  {
       title: "Quiz App",
       image: project4,
       description:
         "A simple app, fetching some questions from an API and displaying them to the user. Features a score counter and a timer.",
       technologies: ["React", "TailwindCSS", "Open Trivia API"],
-      link: ""
+      links: [""
     },*/
 ];
